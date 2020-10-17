83f971e42193072ee941d4f55f6af2fdfbed4594
Lorenzo Gandino <lorenzoga94@gmail.com>
2020-10-17 15:37:45 +0200

    Spostate funzionalita core di app.js in libs/app-utils.js

--- app.js
+++ app.js
@@ -1,37 +1,27 @@
 const http = require('http');
+const apputils = require('./src/libs/app-utils')
+
 const controllers = require('./app.router');
 
-const routers = controllers.reduce((obj, c) => obj.concat(instanceController(c)), []);
-
-const port = '3000';
-
-const server = http.createServer((req, res) => {
-	try {
-		let match = undefined;
-		routers.find(r => {
-			match = req.url.match(r.path);
-			return !!match;
-		}).controller({
-			req,
-			res,
-			match,
-		});
-	} catch (error) {
-		console.error("Exception", error);
-		res.statusCode = 500;
-		res.end();
-	}
-});
-
-server.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}/`);
-});
-
-function instanceController(controller) {
-	let i = controller.$inject !== undefined ? controller.$inject.map(i => getInjectable(k)) : undefined;
-	return controller.apply(null, i);
+const configuration = {
+	port: '3000',
 }
 
-function getInjectable(k) {
-	return undefined;
+const server = http.createServer(
+	apputils.requestServerManager({
+		getControllers: () => controllers,
+		getConsole: () => console,
+		exceptionHandler,
+	})
+);
+
+function exceptionHandler({req, res}, error) {
+	console.error("Exception", error);
+	res.statusCode = 500;
+	res.end();
 }
+
+
+server.listen(configuration.port, () => {
+	console.log(`Server running at http://localhost:${configuration.port}/`);
+});
---
+++ src/libs/app-utils.js
@@ -0,0 +1,43 @@
+function extractRoutersFromControllers(controllers) {
+    return controllers.reduce((obj, c) => obj.concat(instanceController(c)), []);
+}
+
+function requestServerManager({getControllers, getConsole, exceptionHandler}) {
+
+    const aconsole = getConsole();
+    const controllers = getControllers();
+    const routers = extractRoutersFromControllers(controllers);
+
+    return (req, res) => {
+        try {
+            let match = undefined;
+            routers.find(r => {
+                match = req.url.match(r.path);
+                return !!match;
+            }).controller({
+                req,
+                res,
+                match,
+            });
+        } catch (error) {
+            exceptionHandler({
+                req,
+                res,
+            }, error);
+        }
+    }
+}
+
+function instanceController(controller) {
+	let i = controller.$inject !== undefined ? controller.$inject.map(i => getInjectable(k)) : undefined;
+	return controller.apply(null, i);
+}
+
+function getInjectable(k) {
+	return undefined;
+}
+
+module.exports = {
+    requestServerManager,
+    extractRoutersFromControllers,
+};
