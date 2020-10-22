96e1b862f24f1fb5be9ff62e65a2ac8b0af948a4
Lorenzo Gandino <lorenzoga94@gmail.com>
2020-10-22 02:55:09 +0200

    init controller extension

---
+++ src/libs/controller-extension.js
@@ -0,0 +1,40 @@
+module.exports = {
+    reqExtBody,
+    emux,
+    reqExtBodyJSON,
+    pext,
+};
+
+function pext(controller) {
+    const ext = [
+        reqExtBodyJSON,
+    ];
+    return emux(ext)(controller);
+}
+
+function reqExtBody(type) {
+    return controller => function(obj) {
+        let data = '';
+        const req = obj.req;
+        req.on('data', chunk => {
+          data += chunk;
+        })
+        req.on('end', () => {
+            let bodyRequest = undefined
+            bodyRequest = JSON.parse(data);
+            if (type === 'json') {
+            } else {
+                bodyRequest = data;
+            }
+            controller(Object.assign(obj, { bodyRequest }));
+        });
+    }
+}
+
+function reqExtBodyJSON(controller) {
+    return reqExtBody('json')(controller);
+}
+
+function emux(ex) {
+    return ex.reduce((f, x) => (c => f(x(c))));
+}
