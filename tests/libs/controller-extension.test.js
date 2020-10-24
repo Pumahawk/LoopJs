b2d371e82ce08afdc8aa1104dd5b98b61786967f
Lorenzo Gandino <lorenzoga94@gmail.com>
2020-10-24 15:13:42 +0200

    add test for controller-extension.emux

--- tests/libs/controller-extension.test.js
+++ tests/libs/controller-extension.test.js
@@ -2,6 +2,8 @@ const testutils = require('../../src/libs/test-utils');
 const assert = require('assert');
 
 const controllerExtension = require('../../src/libs/controller-extension');
+const { emux } = require('../../src/libs/controller-extension');
+const { SSL_OP_MSIE_SSLV2_RSA_PADDING } = require('constants');
 
 const t = testutils.init({
     name: 'controller-extension',
@@ -28,6 +30,30 @@ function mockRequest(conf) {
     }
 }
 
+function mockMiddleware(conf) {
+    let {
+        preRequest,
+        postRequest,
+    } = conf;
+
+    let ret = {
+        countPre: 0,
+        countPost: 0,
+        middleware: undefined,
+    };
+
+    ret.middleware = controller => obj => {
+        ret.countPre++;
+        preRequest(obj);
+        let retc = controller(obj);
+        ret.countPost++
+        postRequest(retc, obj);
+        return retc;
+    }
+    return ret;
+    
+}
+
 
 function checkBodyRequest(data, onCall) {
     return obj => {onCall(); assert.deepStrictEqual(obj.bodyRequest, data) };
@@ -48,4 +74,48 @@ t('reqExtBody', function() {
         })
     });
     assert.strictEqual(call2, 1);
+});
+
+t('reqExtBodyJSON', function() {
+    let call = 0;
+    controllerExtension.reqExtBodyJSON(checkBodyRequest({value: 'data'}, () => call++))({
+        req: mockRequest({
+            bodyRequest: '{"value": "data"}',
+        })
+    });
+    assert.strictEqual(call, 1);
+})
+
+t('emux', function () {
+    
+    let mid1 = mockMiddleware({
+        preRequest: obj => obj.newAttribute1 = 'mid1', 
+        postRequest: ret => assert.deepStrictEqual(ret, {value: 'test'}),
+    })
+    let mid2 = mockMiddleware({
+        preRequest: obj => obj.newAttribute2 = 'mid2', 
+        postRequest: ret => assert.deepStrictEqual(ret, {value: 'test'}),
+    })
+
+    let rest = emux([
+        mid1.middleware,
+        mid2.middleware,
+    ]);
+
+    function testc(obj) {
+        assert.deepStrictEqual(obj, {
+            newAttribute1: 'mid1',
+            newAttribute2: 'mid2',
+        });
+
+        return {value: 'test'};
+    }
+
+    
+    rest(testc)({});
+    
+    assert.strictEqual(mid1.countPre, 1);
+    assert.strictEqual(mid1.countPost, 1);
+    assert.strictEqual(mid2.countPre, 1);
+    assert.strictEqual(mid2.countPost, 1);
 });
